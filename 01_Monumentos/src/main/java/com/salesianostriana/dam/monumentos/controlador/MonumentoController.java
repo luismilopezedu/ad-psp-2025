3419307fbb4d8da09ed95df71b7d521fffa649c2
Luis Miguel López Magaña <233712211+luismilopezedu@users.noreply.github.com>
2025-11-19 14:12:30 +0100

    Algunos cambios en la documentación

--- 01_Monumentos/src/main/java/com/salesianostriana/dam/monumentos/controlador/MonumentoController.java
+++ 01_Monumentos/src/main/java/com/salesianostriana/dam/monumentos/controlador/MonumentoController.java
@@ -5,12 +5,16 @@ import com.salesianostriana.dam.monumentos.modelo.Monumento;
 import com.salesianostriana.dam.monumentos.modelo.MonumentoResponse;
 import com.salesianostriana.dam.monumentos.servicio.MonumentoServicio;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,8 +28,28 @@ public class MonumentoController {
 
     private final MonumentoServicio servicio;
 
-    @Operation(summary = "Endpoint para obtener todos los monumentos almacenados")
     @GetMapping
+    @Operation(summary = "Endpoint para obtener todos los monumentos almacenados")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Se han encontrado monumentos",
+                    content = @Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = MonumentoResponse.class)),
+                            examples = @ExampleObject(value = """
+                                    [
+                                        {
+                                            "descripcion": "La Giralda de Sevilla es muy bonita porque yo he estado allí",
+                                            "pais": "España"
+                                        }
+                                    ]
+                                    """)
+
+
+                    )
+            )
+    })
     public List<MonumentoResponse> getAll(){
         return servicio.getAll()
                 .stream()
@@ -34,6 +58,37 @@ public class MonumentoController {
                 .toList();
     }
 
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Monumento encontrado",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = MonumentoResponse.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "No se han encontrado monumentos",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ProblemDetail.class),
+                                    examples = @ExampleObject(
+                                            value = """
+                                                    {
+                                                      "type": "http://dam.salesianos-triana.com/monumento-not-found",
+                                                      "title": "Monumento no encontrado",
+                                                      "status": 404,
+                                                      "detail": "No se ha encontrado el monumento con id 1",
+                                                      "instance": "/monumento/1"
+                                                    }
+                                                    """
+                                    )
+                            )
+                    )
+            }
+    )
     @GetMapping("/{id}")
     public ResponseEntity<MonumentoResponse> getById(@PathVariable Long id){
         /*return ResponseEntity.of(
